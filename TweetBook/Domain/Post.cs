1dc7e16ff556251b159a67310c6e04f8bdffe0af
BartoszLitwa <93056426+BartoszLitwa@users.noreply.github.com>
2022-01-24 20:30:41 +0100

    Api Versioning

---
+++ TweetBook/Domain/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace TweetBook.Domain
+{
+    public class Post
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
--- TweetBook/Startup.cs
+++ TweetBook/Startup.cs
@@ -1,18 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TweetBook.Data;
 using TweetBook.Options;
 
